6b43ef574b8c1126fab95f4a30399f164887301a
Mario <mrm994@gmail.com>
2019-08-04 19:54:45 +0200

    added researchable aliases and vim au command to resize focused windows to 120 cols

--- Developer/.scripts/researchable.sh
+++ Developer/.scripts/researchable.sh
@@ -1,3 +1,5 @@
 # Researchable
 alias mvp="cd /Users/mario/Developer/Researchable/MVP/base-platform"
 alias drails="/Users/mario/Developer/Researchable/MVP/base-platform/bin/be rails"
+alias dbundle="/Users/mario/Developer/Researchable/MVP/base-platform/bin/bundle"
+alias dbe="/Users/mario/Developer/Researchable/MVP/base-platform/bin/be"
